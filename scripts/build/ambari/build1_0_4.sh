d174a178b1f5b088b1340964ec5151ac9092a7d5
JaneTTR <14683572+janettr@user.noreply.gitee.com>
2025-01-02 11:44:49 +0800

    feature： ambari 支持 doris 2.1.7

--- scripts/build/ambari/build1_0_4.sh
+++ scripts/build/ambari/build1_0_4.sh
@@ -27,6 +27,7 @@ echo "############## PRE BUILD AMBARI1_0_4 start #############"
 # 定义一个包含所有补丁文件路径的数组
 patch_files=(
   "/scripts/build/ambari/patch1_0_4/patch0-THEME-MISSING-FIXED.dff"
+  "/scripts/build/ambari/patch1_0_4/patch1-DORIS-COMPONENT-ADD.diff"
 )
 PROJECT_PATH="/opt/modules/ambari"
 RPM_PACKAGE="/data/rpm-package/ambari"
